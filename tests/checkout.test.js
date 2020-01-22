63f82f2e7d2cf810b05883063ff421053a193511
Jordi Catala <jordicatalaamat@gmail.com>
2020-01-22 20:27:12 +0100

    initial tests

--- tests/checkout.test.js
+++ tests/checkout.test.js
@@ -1,1 +1,33 @@
-it('is working', () => {});
+describe('Passes Vinterior tests', () => {
+  it('Sums', () => {
+    let co = Checkout(promotional_rules);
+    co.scan({ productCode: '001', name: 'Very Cheap Chair', price: 9.25 });
+    co.scan({ productCode: '002', name: 'Little table', price: 45.0 });
+    expect(co.total).toBe(54.25);
+  });
+
+  it('% discount', () => {
+    let co = Checkout(promotional_rules);
+    co.scan({ productCode: '001', name: 'Very Cheap Chair', price: 9.25 });
+    co.scan({ productCode: '002', name: 'Little table', price: 45.0 });
+    co.scan({ productCode: '003', name: 'Funky light', price: 19.95 });
+    expect(co.total).toBe(66.78);
+  });
+
+  it('Item discount', () => {
+    let co = Checkout(promotional_rules);
+    co.scan({ productCode: '001', name: 'Very Cheap Chair', price: 9.25 });
+    co.scan({ productCode: '003', name: 'Funky light', price: 19.95 });
+    co.scan({ productCode: '001', name: 'Very Cheap Chair', price: 9.25 });
+    expect(co.total).toBe(36.95);
+  });
+
+  it('Item discount + % discount', () => {
+    let co = Checkout(promotional_rules);
+    co.scan({ productCode: '001', name: 'Very Cheap Chair', price: 9.25 });
+    co.scan({ productCode: '002', name: 'Little table', price: 45.0 });
+    co.scan({ productCode: '001', name: 'Very Cheap Chair', price: 9.25 });
+    co.scan({ productCode: '003', name: 'Funky light', price: 19.95 });
+    expect(co.total).toBe(73.76);
+  });
+});
